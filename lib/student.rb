5b58aaac018fd8dda8b4c7c12ecb1b2ad2b90a4b
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2018-12-17 15:13:06 -0500

    Interim checkin

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,72 @@
 require_relative "../config/environment.rb"
 
 class Student
+attr_accessor :name, :grade, :id
 
-  # Remember, you can access your database connection anywhere in this class
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+    # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  end
 
+  def self.create_table
+    sql =<<SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      grade TEXT
+    )
+SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql=<<SQL
+    INSERT INTO students(name, grade)
+    VALUES (?,?)
+SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT * FROM students")[1]
+  end
+
+  def self.create(name, grade)
+      student = self.new(name, grade)
+      student.save
+      student
+  end
+
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
+  def self.find_by_name(name)
+    sql =<<SQL
+    SELECT *
+    FROM songs
+    WHERE name = ?
+SQL
+    results = DB[:conn].execute(sql, name)[0]
+    song.new(result[0], result[1], result[2])
+  end
+
+def update
+  sql =<<SQL
+  UPDATE students
+  SET name = ?
+  GRADE = ?
+SQL
+  DB[:conn].execute(sql, self.name, self.grade)
+end
 
 end
