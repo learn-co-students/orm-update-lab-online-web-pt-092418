2ee4d0ae49866b1c3d7b9472073873d86d4013b6
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2018-12-18 10:29:24 -0500

    Interim checkin

--- lib/student.rb
+++ lib/student.rb
@@ -3,12 +3,10 @@ require_relative "../config/environment.rb"
 class Student
 attr_accessor :name, :grade, :id
 
-  def initialize(id=nil, name, grade)
-    @id = id
+  def initialize(name, grade)
+    @id = nil
     @name = name
     @grade = grade
-    # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
   end
 
   def self.create_table
@@ -28,12 +26,24 @@ SQL
   end
 
   def save
-    sql=<<SQL
+    if self.id 
+      sql=<<SQL
+      UPDATE students
+      set name= ?,
+      grade = ?
+      WHERE id =?
+SQL
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
+    else
+      sql=<<SQL
     INSERT INTO students(name, grade)
     VALUES (?,?)
 SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT * FROM students")[1]
+    all_results = DB[:conn].execute("SELECT id, name, grade FROM students")
+    first_row = all_results[0]
+    @id = first_row[0]
+    end
   end
 
   def self.create(name, grade)
@@ -43,7 +53,7 @@ SQL
   end
 
   def self.new_from_db(row)
-    new_student = self.new
+    new_student = Student.new
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
@@ -57,7 +67,7 @@ SQL
     WHERE name = ?
 SQL
     results = DB[:conn].execute(sql, name)[0]
-    song.new(result[0], result[1], result[2])
+    Student.new(result[0], result[1], result[2])
   end
 
 def update
